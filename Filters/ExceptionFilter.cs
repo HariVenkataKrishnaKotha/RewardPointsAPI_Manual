c6aa883cb7f3ac0a973fbd93ed5dff8bb178d8ac
hvkkotha <hk00016@mix.wvu.edu>
2022-12-21 09:26:55 -0500

    Added the exception filter logic to handle server exceptions in the API

--- Filters/ExceptionFilter.cs
+++ Filters/ExceptionFilter.cs
@@ -1,6 +1,31 @@
-﻿namespace RewardPointsAPI_Manual.Filters
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+namespace RewardPointsAPI_Manual.Filters
 {
-    public class ExceptionFilter
+    public class ExceptionFilter : ExceptionFilterAttribute
     {
+        private readonly ILogger<ExceptionFilter> _logger;
+        public ExceptionFilter(ILogger<ExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        public override void OnException(ExceptionContext context)
+        {
+            // Log the exception
+            _logger.LogError(context.Exception, context.Exception.Message);
+
+            // Set the response status code
+            context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            // Set the response content
+            context.Result = new JsonResult(new
+            {
+                Message = "An error occurred while processing your request."
+            });
+        }
     }
 }
