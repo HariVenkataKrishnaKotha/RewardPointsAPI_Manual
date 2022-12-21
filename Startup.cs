0dcc20415e8f102f93494ff663c1011a71e85248
hvkkotha <hk00016@mix.wvu.edu>
2022-12-21 09:32:13 -0500

    Configured the Startup.cs with Endpoints, Swagger UI support, Logging

--- Startup.cs
+++ Startup.cs
@@ -1,12 +1,57 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog.Extensions.Logging.File;
+using Microsoft.OpenApi.Models;
+using RewardPointsAPI_Manual.Filters;
+using RewardPointsAPI_Manual.Middleware;
 
-namespace RewardPointsAPI_Manual
+namespace RewardsApi
 {
     public class Startup
     {
-        public void Configure(IApplicationBuilder app)
+        public Startup(IConfiguration configuration)
         {
-            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Rewards API", Version = "v1" });
+            });
+            services.AddLogging(configure => configure.AddFile().SetMinimumLevel(Configuration.GetValue<LogLevel>("Logging:File:LogLevel:Default")));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMiddleware<RequestLoggingMiddleware>();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Rewards API V1");
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
